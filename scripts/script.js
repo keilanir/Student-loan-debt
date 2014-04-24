5a966b955b01a0511a1207525321ad2a5df34f2c
keilanir <keilanirodriguez@gmail.com>
2014-04-24 16:37:22 -0400

    Fully functional
    Addition of some elements for user clarity required; styling required

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 // ISSUES:
-// fixing scope of universal variables
 // MATH!!!
 // jQuery UI Sliders - can they bind to text inputs?
 
@@ -9,18 +8,26 @@ var rent;
 var expenses;
 var interest;
 var loans;
-var interest_val;
+var payment;
+var payments;
+var monthly_interest;
+var rate;
+var monthly_rate = 12;
 
 $(document).ready(function(){
-	
-	$('.next').click(function(){
-		$(this).parent('.panel').fadeOut('fast');
-	});
-	
 
-	var rate = .003;
-	var monthly_rate = 12;
-	
+	//Resets form and variable "rate" which sets income = 100% of graph
+	$(':reset').click(function(){
+		// using <form> tags causes problems for toggling buttons
+		// this line uses native Javascript (not jQuery) to reset the 2nd form
+		$('#myForm2')[0].reset()
+		rate = undefined;
+		interest = 0;
+		$('#income_bar').height(0);
+		$('#bar_container').find('div').height(0);
+	});
+
+	// Toggles for interest divs; clears values
 	$('#sub-choices').hide();
 	$('#sub').click(function(){
 		$('#sub-choices').toggle();
@@ -28,21 +35,129 @@ $(document).ready(function(){
 	});
 	$('#unsub-choices').hide();
 	$('#unsub').click(function(){
-		$('#sub-month').find('option:selected').val(0);
-		$('#sub-year').find('option:selected').val(0);
 		$('#unsub-choices').toggle();
 		$('#sub-choices').hide();
+		// Prevents conflict in value of var "interest" by clearing existing
+		$('#sub-month').find('option:selected').val(0);
+		$('#sub-year').find('option:selected').val(0);
 	});
+	
+	// Toggles label descriptions
+	$('.description').hide();
+	$('label').click(function(){
+		$(this).parent('.panel').find('.description').slideToggle('fast');
+	});
+
+	// On any change in any input, run all of the functions called:
+	$('input').change(function(){
+		income = $('#income').val();
+		interest = ($('#interest').val() / 100);
+		loans = $('#loans').val();
+		rent = ($('#rent').val() * monthly_rate);
+		expenses = ($('#expenses').val() * monthly_rate);
+		payment = $('#payments').val();
+		payments = ($('#payments').val() * monthly_rate);
+		monthly_interest = 30 * loans * (interest / 365.25);
+		annual_interest = monthly_interest * 12;
+		animateIncome();
+		animateLoans();
+		animateRent();
+		animateExpenses();
+		animatePayments();
+	});
+	
+	// BUTTON FOR DEBUGGING
+	$('#print').click(function(){
+		$('#wrapper').append(annual_interest + ' ');
+		});
+		
+	var percent_spent = loans / income * 100;
+	$('#percent_spent').empty().append(percent_spent + '%');
+
+// INCOME
+	function animateIncome (){
+		$('#income_bar').find('.label').remove();
+		if (rate == undefined){
+			rate = 300 / income;
+		}else{
+			rate = rate;
+		};
+		var income_height = (income - rent - expenses - payments) * rate;
+		$('#income_bar').animate({
+			'height' : income_height},
+			1000);
+		if (income_height == 0){return;}
+		else {
+			$('#income_bar').append('<p class="label">$' + (income_height / rate) + '</p>');
+		};
+	};
+
+// LOANS
+	function animateLoans (){
+		$('#loans_bar').find('.label').remove();
+		var loans_height = loans * rate;
+		$('#loans_bar').animate({
+			'height' : loans_height},
+			1000);
+		if (loans_height == 0){return;}
+		else {
+			$('#loans_bar').append('<p class="label">$' + (loans_height / rate) + '</p>');
+		};
+	};
+	
+// MONTHLY LOAN PAYMENTS
+	function animatePayments (){
+		$('#payments_bar').find('.label').remove();
+		var payments_height = payments * rate;
+		$('#payments_bar').animate({
+			'height' : payments_height},
+			1000);
+		var principal_paid = Math.round((payment - monthly_interest) * 100) / 100;
+		if (payments_height == 0){return;}
+		else {
+			$('#payments_bar').append('<p class="label">$' + (payments_height / rate) + '</p>');
+		if (payment > 0){
+			$('#principal-paid').empty().append('<span>$' + principal_paid + '</span> of your monthly payment is going toward paying off your principal balance.');
+		}else{return;};
+		};
+	};
+	
+// RENT
+	function animateRent (){
+		$('#rent_bar').find('.label').remove();
+		var rent_height = rent * rate;
+		$('#rent_bar').animate({
+			'height' : rent_height},
+			1000);
+		if (rent_height == 0){return;}
+		else {
+			$('#rent_bar').append('<p class="label">$' + (rent_height / rate) + '</p>');
+		};
+	};
+	
+// EXPENSES
+	function animateExpenses (){
+		$('#expenses_bar').find('.label').remove();
+		var expenses_height = expenses * rate;
+		$('#expenses_bar').animate({
+			'height' : expenses_height},
+			1000);
+		if (expenses_height == 0){return;}
+		else {
+			$('#expenses_bar').append('<p class="label">$' + (expenses_height / rate) + '</p>');
+		}
+	};
 	
 	// additional interest rates: http://www.asa.org/basics/loans/interest-rates/student-loan-interest-rates.aspx
 	
-	// INTEREST:
-	$('#get_interest').click(function(){
+// INTEREST:
+	$('.get-rate').click(function(){
 		var subMonth = $('#sub-month').find('option:selected').val();
 		var subYear = $('#sub-year').find('option:selected').val();
 		interest = $('input:checked').val();
 		if (subMonth <= 7){
 			subYear = subYear - 1;
+			// fix these values:
 			switch (subYear) {
     			case 2006:
     				interest = 6.8
@@ -107,89 +222,10 @@ $(document).ready(function(){
 					alert("Either you have not chosen an interest rate or interest rates for that period aren't available online.")
 					break;
 				};
-			}
+			};
+		$('#interest').val(interest);
+		$('#display-rate').empty().append(interest + '%');
 	});
-		
-	//LOANS function - defines loans
-	$('#loans').change(function (){
-		loans = $('#loans').val();
-	});
-	
-	// INCOME function - takes #income's value and animates #income_bar to match
-	$('#income').change(function (){
-		$('#income_bar').find('.label').remove();
-		income = $('#income').val();
-		var income_height = income * rate;
-		$('#income_bar').animate({
-			'height' : income_height},
-			1000);
-		$('#income_bar').append('<p class="label">$' + income + '</p>');
-
-	});
-	
-	// RENT function - animates #rent_bar and #income_bar
-	$('#rent').change(function (){
-		$('#income_bar').find('.label').remove();
-		$('#rent_bar').find('.label').remove();
-		rent = ($('#rent').val() * monthly_rate);
-		var rent_height = rent * rate;
-		$('#rent_bar').animate({
-			'height' : rent_height},
-			1000);
-		var income_height = (income - rent) * rate;
-		income = income - rent;
-		$('#income_bar').animate({
-			'height' : income_height},
-			1000);
-		$('#income_bar').append('<p class="label">$' + income + '</p>');
-		$('#rent_bar').append('<p class="label">$' + rent + '</p>');
-	});
-	
-	// EXPENSES function - animates #expenses_bar and #income_bar
-	$('#expenses').change(function (){
-		$('#income_bar').find('.label').remove();
-		$('#expenses_bar').find('.label').remove();
-		expenses = ($('#expenses').val() * monthly_rate);
-		var expenses_height = expenses * rate;
-		$('#expenses_bar').animate({
-			'height' : expenses_height},
-			1000);
-		var income_height = (income - expenses) * rate;
-		income = income - expenses;
-		$('#income_bar').animate({
-			'height' : income_height},
-			1000);
-		$('#income_bar').append('<p class="label">$' + income + '</p>');
-		$('#expenses_bar').append('<p class="label">$' + expenses + '</p>');
-	});
-	
-	// LOANS function - animates #loans_bar and #income_bar
-	$('#loans').change(function (){
-		$('#income_bar').find('.label').remove();
-		$('#loans_bar').find('.label').remove();
-		loans = ($('#loans').val() * monthly_rate); // MATH FIX IT
-		var loans_height = loans * rate;
-		$('#loans_bar').animate({
-			'height' : loans_height},
-			1000);
-		var income_height = (income - loans) * rate;
-		income = income - loans;
-		$('#income_bar').animate({
-			'height' : income_height},
-			1000);
-		$('#income_bar').append('<p class="label">$' + income + '</p>');
-		$('#loans_bar').append('<p class="label">$' + loans + '</p>');
-		interest_val = (loans * interest) / 365.25;
-
-	});
-
-	// BUTTON FOR DEBUGGING
-	$('#print').click(function(){
-		$('#wrapper').append(income + ' ');
-		});
-		
-	var percent_spent = loans / income * 100;
-	$('#percent_spent').append(percent_spent);
-
 
 });
+
